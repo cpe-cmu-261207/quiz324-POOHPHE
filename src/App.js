b480a50577512c1f1c676306038c92ba4bd2c60a
FALLENSLAYER007 <37149762+POOHPHE@users.noreply.github.com>
2021-02-09 14:05:41 +0700

    maybe

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import { useState, useEffect } from "react";
 
 function App() {
   //add useState for all state variables
-
+  const [items, setItem] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
   //load locationStorage
   useEffect(() => {
     const items = localStorage.getItem("items");
+    if (items) {
+      console.log(items);
+      const stack = [...items];
+      setItem(stack);
+    }
     // ...
   }, []);
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  function addName() {
+    const fake = [...items];
+    fake.push({ name: name, age: age, gender: gender });
+    setName("");
+    setAge("");
+    setItem(fake);
+  }
 
   return (
     <div className="card" style={{ width: 400 }}>
@@ -20,35 +39,55 @@ function App() {
             className="input"
             type="text"
             placeholder="e.q Coco"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             //update related state based on event
           ></input>
         </div>
 
         <div className="field">
           <label className="label">Gender</label>
-          <select className="input" type="text" placeholder="Please select ..">
+          <select
+            className="input"
+            type="text"
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+            placeholder="Please select .."
+          >
             <option value="" disabled selected hidden>
               -- Select Gender --
             </option>
-            <option>Male</option>
-            <option>Female</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
           </select>
         </div>
 
         <div className="field">
           <label className="label">Age</label>
-          <input className="input" type="number" placeholder="e.q 5"></input>
+          <input
+            className="input"
+            type="number"
+            onChange={(e) => setAge(e.target.value)}
+            value={age}
+            placeholder="e.q 5"
+          ></input>
         </div>
 
-        <button className="button is-danger is-fullwidth">Submit</button>
+        <button className="button is-danger is-fullwidth" onClick={addName}>
+          Submit
+        </button>
 
         <div className="mb-4"></div>
 
         {/* display tables for all persons */}
         <p className="is-4 title has-text-centered">Pet List</p>
         {/* sample table */}
+
         <ItemTable name={"Coco"} gender={"Male"} age={"5"} />
-        <p>Your name and code here</p>
+        {items.map((e) => (
+          <ItemTable name={e.name} gender={e.gender} age={e.age} />
+        ))}
+        <p>Pherawat Wongsawad 620610802</p>
       </div>
     </div>
   );
